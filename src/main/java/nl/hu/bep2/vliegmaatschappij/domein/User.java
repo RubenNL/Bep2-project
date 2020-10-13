d750987f86e96114da3ffa7e56509b1899428f6e
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-10-13 14:37:17 +0200

    todo toegevoegd aan User

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/User.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/User.java
@@ -1,6 +1,7 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User extends Persoon {
@@ -9,6 +10,7 @@ public class User extends Persoon {
     private int id;
     @Enumerated(EnumType.STRING)
     private Role role;
+    // TODO: List relatie met Boeking
 
     public User(String voornaam, String achernaam, int leeftijd, String email, int telefoon, String nationaliteit, Role role) {
         super(voornaam, achernaam, leeftijd, email, telefoon, nationaliteit);
