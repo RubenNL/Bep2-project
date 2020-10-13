b0aaf11eadba4818b5ef4f0e17936ea712667561
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-10-13 11:31:05 +0200

    fixes #B2-35

---
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/presentation/RedirectToIndex.java
@@ -0,0 +1,12 @@
+package nl.hu.bep2.vliegmaatschappij.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RedirectToIndex {
+	@RequestMapping("")
+	public String homePage() {
+		return "index.html";
+	}
+}
