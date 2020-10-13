75ce77362c7cf9e4e7bfc8e31732992cf957140a
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-10-13 13:50:09 +0200

    Klant hernoemd naar User, User enum geannoteerd

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Role.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Role.java
@@ -1,8 +1,6 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
-import javax.persistence.Entity;
-
 public enum Role {
-    KLANT,
-    MEDEWERKER
+    CUSTOMER,
+    EMPLOYEE
 }
--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/User.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/User.java
@@ -1,17 +1,16 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Klant extends Persoon {
+public class User extends Persoon {
     @Id
     @GeneratedValue
     private int id;
+    @Enumerated(EnumType.STRING)
     private Role role;
 
-    public Klant(String voornaam, String achernaam, int leeftijd, String email, int telefoon, String nationaliteit, Role role) {
+    public User(String voornaam, String achernaam, int leeftijd, String email, int telefoon, String nationaliteit, Role role) {
         super(voornaam, achernaam, leeftijd, email, telefoon, nationaliteit);
         this.role = role;
     }
