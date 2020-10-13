0a884b8c45de84581d5fa102f2041b6a43e0142b
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2020-10-13 14:41:47 +0200

    spaces naar tabs

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Prijs.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Prijs.java
@@ -6,25 +6,25 @@ import javax.persistence.Id;
 
 @Entity
 public class Prijs {
-    @Id
-    @GeneratedValue
-    private int id;
-    private double prijs;
+	@Id
+	@GeneratedValue
+	private int id;
+	private double prijs;
 
-    public Prijs(Vliegtuigtype type, Klasse klasse, Vluchtroute route) {
-        this.prijs = berekenPrijs(type,klasse,route);
-    }
+	public Prijs(Vliegtuigtype type, Klasse klasse, Vluchtroute route) {
+		this.prijs = berekenPrijs(type,klasse,route);
+	}
 
-    public double berekenPrijs(Vliegtuigtype type, Klasse klasse, Vluchtroute route){
-        // Hier een calculatie maken voor de prijs
-        return 0.0;
-    }
+	public double berekenPrijs(Vliegtuigtype type, Klasse klasse, Vluchtroute route){
+		// Hier een calculatie maken voor de prijs
+		return 0.0;
+	}
 
-    public double getPrijs() {
-        return prijs;
-    }
+	public double getPrijs() {
+		return prijs;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 }
