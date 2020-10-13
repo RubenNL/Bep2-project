2206fb08c6ffe110737968cd0347272401628b3f
Dlankheet <daniel.lankheet@hotmail.com>
2020-10-13 15:39:11 +0200

    Vliegtuigtype Vliegtuig aangemaakt
    Took 1 hour 18 minutes

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Vliegveld.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Vliegveld.java
@@ -1,10 +1,10 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
 public class Vliegveld {
-    private String code;
-    private String naam;
-    private double lat;
-    private double lng;
-    private String stad;
-    private String land;
+	private String code;
+	private String naam;
+	private double lat;
+	private double lng;
+	private String stad;
+	private String land;
 }
