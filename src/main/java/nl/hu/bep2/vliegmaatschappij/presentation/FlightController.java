452ad8b6136075654ea11ec141dfa04a3d8aca83
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-10-19 14:15:23 +0200

    HateOAS

--- src/main/java/nl/hu/bep2/vliegmaatschappij/presentation/FlightController.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/presentation/FlightController.java
@@ -1,50 +1,71 @@
 package nl.hu.bep2.vliegmaatschappij.presentation;
 
-import nl.hu.bep2.vliegmaatschappij.application.FlightService;
-import nl.hu.bep2.vliegmaatschappij.domein.Airport;
+import nl.hu.bep2.vliegmaatschappij.data.SpringFlightRepository;
 import nl.hu.bep2.vliegmaatschappij.domein.Flight;
-import nl.hu.bep2.vliegmaatschappij.exceptions.AirportNotFoundException;
 import nl.hu.bep2.vliegmaatschappij.exceptions.FlightNotFoundException;
-import nl.hu.bep2.vliegmaatschappij.presentation.dto.FlightDTO;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
-	private final FlightService service;
+    private final SpringFlightRepository repository;
+    private final FlightModelAssembler assembler;
 
-	public FlightController(FlightService service) {
-		this.service = service;
-	}
+    public FlightController(SpringFlightRepository repository, FlightModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+    @PostMapping
+    ResponseEntity<?> newFlight(@RequestBody Flight flight) {
+        EntityModel<Flight> entityModel = assembler.toModel(repository.save(flight));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
+    @GetMapping("/{id}")
+    EntityModel<Flight> one(@PathVariable int id) {
+        Flight flight = repository.findById(id)
+                .orElseThrow(() -> new FlightNotFoundException("flight not found"));
 
-	@PostMapping
-	public int createFlight(@Validated @RequestBody FlightDTO flight) {
-		return this.service.createFlight(
-				flight.departureTime,
-				flight.arrivalTime
-		).getId();
-	}
+        return assembler.toModel(flight);
+    }
+    @GetMapping("/all")
+    CollectionModel<EntityModel<Flight>> all() {
+        List<EntityModel<Flight>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees, linkTo(methodOn(FlightController.class).all()).withSelfRel());
+    }
+    @PutMapping("/{id}")
+    ResponseEntity<?> replaceEmployee(@RequestBody Flight newFlight, @PathVariable int id) {
+        Flight updatedFlight = repository.findById(id)
+                .map(flight -> {
+                    flight.setarrivalTime(newFlight.getarrivalTime());
+                    flight.setdepartureTime(newFlight.getdepartureTime());
+                    return repository.save(flight);
+                })
+                .orElseGet(() -> {
+                    newFlight.setId(id);
+                    return repository.save(newFlight);
+                });
+        EntityModel<Flight> entityModel = assembler.toModel(updatedFlight);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+    }
 
-	@GetMapping("/{id}")
-	public FlightDTO showFlight(@PathVariable int id) throws FlightNotFoundException {
-		Flight flight = service.showFlight(id);
-		return new FlightDTO(flight);
-	}
-
-    /*@PostMapping("/{id}")
-    public void updateFlight(@Validated @RequestBody Flight flight){
-        this.service.createFlight(
-                flight.getId(),
-                flight.getdepartureTime(),
-                flight.getarrivalTime()
-        );
-    }*/
-
-	@DeleteMapping("/{id}")
-	public void deleteFlight(@PathVariable int id) {
-		this.service.delete(id);
-	}
-
-
+    @DeleteMapping("/{id}")
+    public void deleteFlight(@PathVariable int id) {
+        repository.deleteById(id);
+    }
 }
