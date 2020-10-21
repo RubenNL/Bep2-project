ff208a74cfdb2c7084e7cef52140061594309f4e
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-10-21 09:55:00 +0200

    AirportBoekingLombok

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Booking.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Booking.java
@@ -1,9 +1,16 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Booking {
 	@Id
 	@GeneratedValue
@@ -14,44 +21,4 @@ public class Booking {
 	private Customer customer;
 	@OneToOne
 	private Price price;
-
-	public Booking() {
-	}
-
-	public Booking(List<Person> persons, Price price) {
-		this.persons = persons;
-		this.price = price;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public List<Person> getPersons() {
-		return persons;
-	}
-
-	public void setPersons(List<Person> persons) {
-		this.persons = persons;
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
-
-	public Price getPrice() {
-		return price;
-	}
-
-	public void setPrice(Price price) {
-		this.price = price;
-	}
 }
