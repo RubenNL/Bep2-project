2cae4d6616d2debb5d5c16a83f55c05563d2e7d6
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-10-21 10:00:01 +0200

    PersonPlaneLombok

--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Person.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Person.java
@@ -1,10 +1,17 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
 	@Id
 	@GeneratedValue
@@ -15,73 +22,4 @@ public class Person {
 	protected String email;
 	protected int phone;
 	protected String nationality;
-
-	public Person() {
-
-	}
-
-	public Person(String firstName, String lastName, int age, String email, int phone, String nationality) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.email = email;
-		this.phone = phone;
-		this.nationality = nationality;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String voornaam) {
-		this.firstName = voornaam;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String achernaam) {
-		this.lastName = achernaam;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int leeftijd) {
-		this.age = leeftijd;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public int getPhone() {
-		return phone;
-	}
-
-	public void setPhone(int telefoon) {
-		this.phone = telefoon;
-	}
-
-	public String getNationality() {
-		return nationality;
-	}
-
-	public void setNationality(String nationaliteit) {
-		this.nationality = nationaliteit;
-	}
 }
--- src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Plane.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/domein/Plane.java
@@ -1,37 +1,20 @@
 package nl.hu.bep2.vliegmaatschappij.domein;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Plane {
 	@Id
 	private String code;
 	@ManyToOne
 	private Planetype type;
-
-	public Plane(String code){
-		this.code = code;
-	}
-
-	public Plane() {
-
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public Planetype getType() {
-		return type;
-	}
-
-	public void setType(Planetype type) {
-		this.type = type;
-	}
 }
