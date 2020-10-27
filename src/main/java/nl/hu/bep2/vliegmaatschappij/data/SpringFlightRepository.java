0f6f74b481964b87f72e34428cfc26bdfe4a0cc0
Dlankheet <daniel.lankheet@hotmail.com>
2020-10-27 13:39:38 +0100

    fix mergeshizzle springrepository
    Took 2 minutes

--- src/main/java/nl/hu/bep2/vliegmaatschappij/data/SpringFlightRepository.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/data/SpringFlightRepository.java
@@ -13,10 +13,6 @@ public interface SpringFlightRepository extends JpaRepository<Flight, Integer> {
 	@Query("SELECT flight FROM Flight flight WHERE flight.route.departure.code= :depart AND flight.route.destination.code= :destination AND flight.departureTime BETWEEN :start AND :end")
 	List<Flight> findByFlight(@Param("depart") String depart, @Param("destination") String destination, @Param("start") LocalDateTime start,@Param("end") LocalDateTime end);
 
-<<<<<<< HEAD
 	@Query("SELECT flight FROM Flight flight WHERE flight.departureTime > :querytime ORDER BY flight.departureTime ASC")
-=======
-	@Query("SELECT flight FROM Flight flight WHERE flight.departureTime > :querytime")
->>>>>>> 8a2aaa758b3a06cf39a1052e9a3144f398afbf1c
 	List<Flight> findByTime(@Param("querytime") LocalDateTime datetime);
 }
