88ba8527f2ac3d57ea2e0b36971d2f88209b2829
Daniel Lankheet <daniel.lankheet@hotmail.com>
2020-11-04 14:04:56 +0100

    Update SecurityConfig.java

--- src/main/java/nl/hu/bep2/vliegmaatschappij/security/SecurityConfig.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET,"/v3/**").permitAll()
 				.antMatchers(HttpMethod.GET,"/swagger-ui/**").permitAll()
 				.antMatchers(HttpMethod.GET,"/booking/confirm/*").permitAll()
+				.antMatchers(HttpMethod.GET,"/redirect.html").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.addFilterBefore(
