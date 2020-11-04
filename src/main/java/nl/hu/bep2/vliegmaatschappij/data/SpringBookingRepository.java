de607a720962dbccfbb0fb96d50a2f7611eab729
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-11-04 10:11:02 +0100

    random ID for booking

--- src/main/java/nl/hu/bep2/vliegmaatschappij/data/SpringBookingRepository.java
+++ src/main/java/nl/hu/bep2/vliegmaatschappij/data/SpringBookingRepository.java
@@ -3,5 +3,5 @@ package nl.hu.bep2.vliegmaatschappij.data;
 import nl.hu.bep2.vliegmaatschappij.domein.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringBookingRepository extends JpaRepository<Booking, Integer> {
+public interface SpringBookingRepository extends JpaRepository<Booking, String> {
 }
